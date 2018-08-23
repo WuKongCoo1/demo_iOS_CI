2eeec73bd38663ba9e2a8d4db8ac2928c8589e11
Jacky <wu_kong_coo1@163.com>
2018-08-23 19:16:08 +0800

    创建示例工程

--- demo_xocdebuild/build/demo_xocdebuild.build/Release-iphoneos/demo_xocdebuild.build/Objects-normal/arm64/ViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/jacky/文稿/work/Share/命令行打包分享/demo_xocdebuild/demo_xocdebuild/ViewController.m \
-  /Users/jacky/文稿/work/Share/命令行打包分享/demo_xocdebuild/demo_xocdebuild/ViewController.h
